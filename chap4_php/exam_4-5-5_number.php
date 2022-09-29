18f92ca3289ea6e48b35d53f9f3963de83a7bbed
ko9un <ktw1633@gmail.com>
2022-09-29 16:09:35 +0900

    수정

--- chap4_php/exam_4-5-5_number.php
+++ chap4_php/exam_4-5-5_number.php
@@ -1,12 +1,15 @@
 <!DOCTYPE>
 <html>
-    <body>
-        <h1>Number & Arithmetic Operator</h1>
-        
 
-        <h2>4/2</h2>
-        <?php
-        echo 4/2;
-        ?>
+<body>
+    <h1>Number & Arithmetic Operator</h1>
+
+ 
+    <h2>4/2</h2>
+    <?php
+    echo 4/2;
+
+    ?>
+
     </body>
 </html>
