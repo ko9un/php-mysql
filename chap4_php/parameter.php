bf3da7a8d257daef52393429f5a9872fc702995b
ko9un <ktw1633@gmail.com>
2022-09-29 16:08:15 +0900

    .

---
+++ chap4_php/parameter.php
@@ -0,0 +1,15 @@
+<?php
+ echo $title = $_GET['title'];
+ echo $address = $_GET['address'];
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    </head>
+    <body>
+       <?=$address?>에 사시는  <?=$title?> 님
+    </body>
+</html>
---
+++ chap4_php/variable.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>Variable</h1>
+        <?php
+            $name = "leezche";
+            echo "ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ".$name."ㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴ".$name."dddddddddddddddddddddddd".$name;
+        
+        ?>
+    </body>
+</html>
