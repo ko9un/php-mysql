f35902ee46413b95380299c5f49eb73e2b71ec9a
ko9un <ktw1633@gmail.com>
2022-09-29 12:19:22 +0900

    php 데이터 타입

---
+++ chap4_php/exam_4-5-5_number.php
@@ -0,0 +1,12 @@
+<!DOCTYPE>
+<html>
+    <body>
+        <h1>Number & Arithmetic Operator</h1>
+        
+
+        <h2>4/2</h2>
+        <?php
+        echo 4/2;
+        ?>
+    </body>
+</html>
---
+++ chap4_php/exam_4_5_4_number.php
@@ -0,0 +1,12 @@
+<!DOCTYPE>
+<html>
+    <body>
+        <h1>Number & Arithmetic Operator</h1>
+        
+
+        <h2>2*2</h2>
+        <?php
+        echo 2*2;
+        ?>
+    </body>
+</html>
