0c4a8207164947f2495904f35cfe342411df1dcf
ko9un <ktw1633@gmail.com>
2022-09-30 15:04:16 +0900

    조건문의형식

---
+++ conditional.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Conditional</h1>
+        <h2>if/else</h2>
+        <?php
+            echo '1<br>';
+            if(true){
+            echo '2-1<br>';
+            }else{
+                echo '2-2<br>';
+            }
+            echo '3<br>';
+        ?>
+        
+        
+    </body>
+</html>
